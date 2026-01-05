8f025c212f2d35da0dfbafa30f79d6e3b5df9f65
Azharul Islam Nayif <azharul151965@gmail.com>
2026-01-05 16:59:01 +0600

    new commit

--- BMI_Calculator.dart
+++ BMI_Calculator.dart
@@ -27,10 +27,10 @@ String Bmi_category (double bmi){
 
 void main(){
   double Bmi=(Bmi_calculation(80,1.75));
-  String c=Bmi_category(Bmi);
+  String category =Bmi_category(Bmi);
 
   print("Your BMI is:${Bmi.toStringAsFixed(2)}\n"
-      "Category:$c  "
+      "Category:$category  "
        );
 
 }
--- set_print.dart
+++ set_print.dart
@@ -1,9 +1,9 @@
-import 'package:test/expect.dart';
+
 void main(){
   Set<int>A={1,2,3,4,5};
   Set<int>B={4,5,6,7,8};
-  Set<int>s3=(A.union(B));
+  Set<int>C=(A.union(B));
 
-  for(int item in s3){
+  for(int item in C){
     print(item);
   }}
